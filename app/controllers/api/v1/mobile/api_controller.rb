9e750413a5af36dd33128c00c7227e2fc1f6e171
Jesen92 <hjesenovic@gmail.com>
2017-06-13 23:27:59 +0200

    Dodan respond_to :json za api

--- app/controllers/api/v1/mobile/api_controller.rb
+++ app/controllers/api/v1/mobile/api_controller.rb
@@ -3,6 +3,7 @@ module Api
     module Mobile
       class ApiController < ApplicationController
         include JsonApiResponders
+        respond_to :json
         skip_before_action  :verify_authenticity_token, :authenticate_user!
         before_action :configure_permitted_parameters, if: :devise_controller?
 
